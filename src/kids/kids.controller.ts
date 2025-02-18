624a8c859eca752f0ca7f5a4717cdfe1a18a6181
Geszler <geszler.matr@gmail.com>
2025-02-18 01:07:07 +0100

    végleges

--- src/kids/kids.controller.ts
+++ src/kids/kids.controller.ts
@@ -2,76 +2,81 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } 
 import { KidsService } from './kids.service';
 import { CreateKidDto } from './dto/create-kid.dto';
 import { UpdateKidDto } from './dto/update-kid.dto';
-import { ApiBadRequestResponse, ApiResponse, ApiParam } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiResponse, ApiParam, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Kid } from './entities/kid.entity';
 
+@ApiTags('kids')
 @Controller('kids')
 export class KidsController {
   constructor(private readonly kidsService: KidsService) {}
 
   /**
-   * Adds new kid database
+   * Adds a new kid to the database
    * 
-   * @param createVideoDto The data of the new kid
-   * @returns All of the data, including the generated fields
+   * @param createKidDto The data of the new kid
+   * @returns The created kid data, including generated fields
    */
-
   @Post()
-  @ApiResponse({
-    status:201,
-    description: "The kid was created succesfully",
-    type:Kid,
-    example: {
-      id:1,
-      name:"Kiss István",
-      address:'2146 Mogyoród, Kossuth utca 13',
-      wasGood: true,
-      wantedToy: 'Mobilephone'
-    }
-  })
-
-  @ApiBadRequestResponse({
-    description: 'A validation error occured'
-  })
-
-  create(@Body() createKidDto: CreateKidDto) {
+  @ApiOperation({ summary: 'Create a new kid' })
+  create(@Body() createKidDto: CreateKidDto): Promise<Kid> {
     return this.kidsService.create(createKidDto);
   }
 
+  /**
+   * Retrieves all kids from the database
+   * 
+   * @returns An array of all kids
+   */
   @Get()
-  findAll() {
+  @ApiOperation({ summary: 'Retrieve all kids' })
+  findAll(): Promise<Kid[]> {
     return this.kidsService.findAll();
   }
 
-   /**
-   * Returns the data of a given video
+  /**
+   * Retrieves a kid by their ID
    * 
-   * @param id The ID of the video
-   * @returns 
+   * @param id The ID of the kid
+   * @returns The data of the specified kid
    */
-
-   @ApiParam({
-    name: 'id',
-    description: 'The ID of the kid',
-    type: 'number'
-  })
-
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  @ApiOperation({ summary: 'Retrieve a kid by ID' })
+  @ApiParam({ name: 'id', description: 'The ID of the kid', type: 'number' })
+  async findOne(@Param('id') id: string): Promise<Kid> {
     const kid = await this.kidsService.findOne(+id);
-    if (kid == null) {
-      throw new NotFoundException("Nincs ilyen gyerek")
+    if (!kid) {
+      throw new NotFoundException('Kid not found');
     }
     return kid;
   }
 
+  /**
+   * Updates a kid's data by their ID
+   * 
+   * @param id The ID of the kid
+   * @param updateKidDto The new data for the kid
+   * @returns The updated kid data
+   */
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateKidDto: UpdateKidDto) {
+  @ApiOperation({ summary: 'Update a kid by ID' })
+  @ApiParam({ name: 'id', description: 'The ID of the kid', type: 'number' })
+  update(
+    @Param('id') id: string,
+    @Body() updateKidDto: UpdateKidDto,
+  ): Promise<Kid> {
     return this.kidsService.update(+id, updateKidDto);
   }
 
+  /**
+   * Deletes a kid by their ID
+   * 
+   * @param id The ID of the kid
+   * @returns A confirmation message
+   */
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.kidsService.remove(+id);
+  @ApiOperation({ summary: 'Delete a kid by ID' })
+  @ApiParam({ name: 'id', description: 'The ID of the kid', type: 'number' })
+  async remove(@Param('id') id: string): Promise<void> {
+    await this.kidsService.remove(+id); 
   }
 }
